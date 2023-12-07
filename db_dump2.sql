60af74904341a822844a61a8db534bb41b1208a0
bf4680 <145667488+bf4680@users.noreply.github.com>
2023-12-07 11:41:47 +0100

    Add files via upload
    Updated data base dump with cars table and 1 mercedes car

---
+++ db_dump2.sql
@@ -0,0 +1,60 @@
+/*
+SQLyog Community v12.4.3 (32 bit)
+MySQL - 8.0.35 : Database - app_dev
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`app_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `app_dev`;
+
+/*Table structure for table `cars` */
+
+DROP TABLE IF EXISTS `cars`;
+
+CREATE TABLE `cars` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Brand` varchar(26) NOT NULL,
+  `Km` int NOT NULL DEFAULT '0',
+  `Price` double NOT NULL,
+  `Color` varchar(26) NOT NULL,
+  `Fuel` enum('petrol','diesel','electric') NOT NULL,
+  `Gear_shift` enum('manual','automatic') NOT NULL,
+  `Sold` tinyint(1) NOT NULL,
+  KEY `ID` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `cars` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(26) NOT NULL,
+  `LastName` varchar(26) NOT NULL,
+  `mail` varchar(26) NOT NULL,
+  `User_type` enum('user','admin') NOT NULL,
+  `Valid` tinyint(1) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  KEY `ID` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
+/*Data for the table `users` */
+
+insert  into `users`(`ID`,`FirstName`,`LastName`,`mail`,`User_type`,`Valid`,`Password`) values 
+(1,'Balta','Fernandez','baltifg@gmail.com','admin',1,'rit2023');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
